2dbccad3fdec40e42e44993d2010e3cc6098e9d0
sebm <sebastian.m@gmail.com>
2010-12-12 20:06:29 +0000

    JS Datelist

--- myapp/actions.py
+++ myapp/actions.py
@@ -19,7 +19,15 @@ class IndexHandler(webapp.RequestHandler):
 	def get(self):
 		a = AnalysesHandler()
 
+		analyses_gdata = \
+							a.getGDataForAnalyses(a.getDaysOfAnalysisForPlace('Toronto', 7)) + \
+							a.getGDataForAnalyses(a.getDaysOfAnalysisForPlace('NYC', 7)) + \
+							a.getGDataForAnalyses(a.getDaysOfAnalysisForPlace('London', 7)) + \
+							a.getGDataForAnalyses(a.getDaysOfAnalysisForPlace('Margate', 7)) + \
+							a.getGDataForAnalyses(a.getDaysOfAnalysisForPlace('Oxford', 7))
+							
 		template_values = {
+			'gdata': analyses_gdata,
 			'TorontoSentiment': a.getAnalysisForPlace('Toronto'), 
 			'OxfordSentiment': a.getAnalysisForPlace('Oxford'), 
 			'LondonSentiment': a.getAnalysisForPlace('London'), 
