5e5157879ead8eb91abde1f55eebbee7aef959e8
sebm <sebastian.m@gmail.com>
2011-03-12 15:03:23 +0000

    How many analyses are there?

--- myapp/TidyUpHandler.py
+++ myapp/TidyUpHandler.py
@@ -26,9 +26,9 @@ class TidyUpHandler(webapp.RequestHandler):
         counter +=1
         
     elif datatype == "analysis":
-      num_stale_analyses = Analysis.gql('WHERE created_at < :before', before=one_month_ago ).count()
+      num_all_analyses = Analysis.all(keys_only=True).count()
       old_analyses = Analysis.gql('WHERE created_at < :before LIMIT 1000', before=one_month_ago )
-      self.response.out.write("There are %s stale analyses in total<br>" % num_stale_analyses)
+      self.response.out.write("There are %s analyses in total<br>" % num_all_analyses)
       for a in old_analyses:
         a.delete()
         counter += 1
